449f061fe0162225548ce30a71a24c7ad068d81d
maewec <dmitrij_n@inbox.ru>
2020-09-18 07:10:35 +0300

    Новый проект

---
+++ hypnose.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import tkinter as tk
+
+class Circle:
+    def __init__(self, canvas, x0, y0, radius, thickness):
+        pass
+
+
+root = tk.Tk()
+canv = tk.Canvas(root, width=200, height=200, bg='white')
+canv.pack()
+#fig = Circle()
+
+
+root.mainloop()
