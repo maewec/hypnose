9c610bc5e0542a97d8bcd5f7e2adaa6ad40b090d
maewec <dmitrij_n@inbox.ru>
2020-09-22 07:48:29 +0300

    Создал класс окружности и добавил метод изменения радиуса

--- hypnose.py
+++ hypnose.py
@@ -3,13 +3,13 @@
 import tkinter as tk
 
 class Circle:
-    def __init__(self, canvas, center, radius, thickness, color='green'):
+    def __init__(self, canvas, center, radius, thickness, color='green', inc=10):
         self.canvas = canvas
         self.center = center
         self.radius = radius
         self.thickness = thickness
-        self.start = 0
         self.color = color
+        self.inc=inc
         self.create()
 
     def create(self):
@@ -18,9 +18,13 @@ class Circle:
         x1 = x + self.radius
         y0 = y - self.radius
         y1 = y + self.radius
-        self.oval = self.canvas.create_oval(x0, y0, x1, y1, start=self.start,
-                                          extent=self.ang, style='arc',
-                                          outline=self.color, width=self.thickness)
+        self.oval = self.canvas.create_oval(x0, y0, x1, y1, width=self.thickness,
+                                           outline=self.color)
+
+    def increase(self):
+        self.canvas.delete(self.oval)
+        self.radius += self.inc
+        self.create()
 
     def rotate(self, angle=None):
         if self.start > 360:
@@ -33,13 +37,18 @@ class Circle:
         self.create()
 
     def __del__(self):
-        self.canvas.delete(self.arc)
+        self.canvas.delete(self.oval)
 
+def increase(event, obj):
+    obj.increase()
 
 root = tk.Tk()
 canv = tk.Canvas(root, width=600, height=600, bg='white')
 canv.pack()
+but = tk.Button(root, text='Расширить')
+but.pack()
 
-arc = Arc(canv, (300, 300), 100, 10, 365, 'blue', 10)
+oval = Circle(canv, (300, 300), 100, 10, 'blue')
 
+but.bind('<Button-1>', lambda event: increase(event, oval))
 root.mainloop()
